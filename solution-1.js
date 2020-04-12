f6a6ebd3cf6f847c96fcd03c2c61b87553097fc1
George Arnold <george.arnold.88@gmail.com>
2020-04-12 14:50:44 -0400

    initial commit

---
+++ solution-1.js
@@ -0,0 +1,14 @@
+function partition(array, start, end) {
+  const pivot = array[end - 1];
+  let j = start;
+  for (let i = start; i < end - 1; i++) {
+    if (array[i] <= pivot) {
+      swap(array, i, j);
+      j++;
+    }
+  }
+  swap(array, end - 1, j);
+  return j;
+}
+
+console.log(partition([1, 2, 3, 4, 8, 10, 12, 20]));
