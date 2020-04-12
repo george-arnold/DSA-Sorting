0fa19969faab9bb349828b030bbda70a69affeea
George Arnold <george.arnold.88@gmail.com>
2020-04-12 16:09:20 -0400

    answers

--- solution-1.js
+++ solution-1.js
@@ -1,14 +1,10 @@
-function partition(array, start, end) {
-  const pivot = array[end - 1];
-  let j = start;
-  for (let i = start; i < end - 1; i++) {
-    if (array[i] <= pivot) {
-      swap(array, i, j);
-      j++;
-    }
-  }
-  swap(array, end - 1, j);
-  return j;
-}
+// Given the following list of numbers 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
 
-console.log(partition([1, 2, 3, 4, 8, 10, 12, 20]));
+//     What is the resulting list that will be sorted after 3 recursive calls to mergesort?
+1, 21, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40;
+//     What is the resulting list that will be sorted after 16 recursive calls to mergesort?
+1, 2, 9, 21, 22, 26, 29, 45, 16, 27, 34, 39, 40, 43, 46, 49;
+//     What are the first 2 lists to be merged?
+// 1, 21, and 26, 45
+//     Which two lists would be merged on the 7th merge?
+// 1, 21, 26, 45 and 29, 28, 2, 9
---
+++ solution-2.js
@@ -0,0 +1,6 @@
+// 1) Suppose you are debugging a quicksort implementation that is supposed to sort an array in ascending order.
+//  After the first partition step has been completed, the contents of the array is in the following order:
+//  3 9 1 14 17 24 22 20.
+//  Which of the following statements is correct about the partition step? Explain your answer.
+
+// Both 14 and 17 coould be the pivot because the values to the left and right of it are correctly displayed every number left is lower and right is higher
